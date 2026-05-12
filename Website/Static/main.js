39e38b5d8fa663bc77fb83b7b501c94f671dafaa
George Vaughan <georgevaughan135@gmail.com>
2026-05-12 17:13:47 +1200

    buttons, updating js icon, corrected scaling
    As described, added icon with js that scales and can be edited with a new link to be updated (for spotify/tidal pfp support) and fixed much of the scaling, and added buttons for the navigation box but they dont work yet

---
+++ Website/Static/main.js
@@ -0,0 +1,7 @@
+
+const userIcon = document.createElement("img");
+userIcon.src = "assets/userIcon.png";
+userIcon.alt = "User icon";
+userIcon.style.width = '100%'
+userIcon.style.height = '100%'
+document.getElementById("userIcon").appendChild(userIcon);
