3471d82fb59773daecfb1069ae0b8031c057c8a5
George Vaughan <georgevaughan135@gmail.com>
2026-05-19 18:00:57 +1200

    Created text input for the chatbox, flask next

--- _Flask Backend_/app.py
+++ _Flask Backend_/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
+
+
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/items', methods=['GET'])
+def GetItems():
+    return jsonify(list(items.values())), 200
+
 
 
 if __name__ == "__main__":
